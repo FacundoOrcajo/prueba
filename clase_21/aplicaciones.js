b175de4c98b01c2cc908ef7090a03457235f659c
FacundoOrcajo <facundo20orcajo@gmail.com>
2022-06-02 11:44:39 -0300

    Agregué un nuevo archivo

---
+++ clase_21/aplicaciones.js
@@ -0,0 +1,12 @@
+let aplicaciones = [
+    {
+        app : instagram,
+        version : 3.7,
+        anio : 2013,
+    },
+    {
+        app : whatssap,
+        version : 2.8,
+        anio : 2012,
+    }
+]
